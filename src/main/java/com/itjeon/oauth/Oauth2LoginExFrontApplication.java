d373f9f9bb4a360f221105fad017be71c43a49d1
itjeon <itjeon@inspace.re.kr>
2023-05-23 16:35:00 +0900

    init commit

---
+++ src/main/java/com/itjeon/oauth/Oauth2LoginExFrontApplication.java
@@ -0,0 +1,13 @@
+package com.itjeon.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LoginExFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2LoginExFrontApplication.class, args);
+	}
+
+}
